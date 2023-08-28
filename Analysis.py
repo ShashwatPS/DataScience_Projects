408b9ad3344dae313805e73ba2ba4e1a264d5065
Shashwat Singh <shashwat123student@gmail.com>
2023-08-29 00:45:59 +0530

    Gonna learn Apply Method now

--- Analysis.py
+++ Analysis.py
@@ -27,6 +27,18 @@ df1=pd.DataFrame(dict1)
 # print(df1.describe()) By default it displays statistics for numerical columns only
 # print(df1.describe(include='all')) This include='all' forces it to include all the columns
 
+# print(df1['Gender'].unique()) Returns the unique values in that particular column which in this case is ['Male','Female'] in gender column
 
+# print(df1['Gender'].nunique()) This returns the no of unique value in that particular not those particular values
 
+# print(df1['Gender'].value_counts()) It displays the count of unique Values
+
+# print(df1[df1['Marks']>=90]) Displays those students having marks >=90 in the 'Marks' column
+# print(df1[(df1['Marks']>=90) & (df1['Marks']<=100)]) Displays those students having marks in between 90 and 100 (inclusive)
+# print(df1['Marks'].between(90,100)) By default the students having marks in between 90 and 100 (inclusive) in the 'Marks' column
+# print(sum(df1['Marks'].between(90,100))) It displays the no.of students having marks in between 90 and 100 (inclusive) in the 'Marks' column
+
+# print(df1['Marks'].mean()) Returns the mean of that particular column works only for numeric data.
+# print(df1['Marks'].min()) Returns the minimum value in that particular column
+# print(df1['Marks'].min()) Returns the maximum value in that particular column
 
