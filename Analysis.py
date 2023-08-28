6222e4c1597e18f288c0e1f43464d2421b43a2a6
Shashwat Singh <shashwat123student@gmail.com>
2023-08-28 23:59:03 +0530

    Added Basic commands

--- Analysis.py
+++ Analysis.py
@@ -8,4 +8,10 @@ dict1 ={'Name':['Priyang','Aadhya','Krisha','Vedant','Parshv',
                }
 df1=pd.DataFrame(dict1)
 
-print(df1)
+# print(df1) Prints the Database
+
+# print(df1.head(x)) Prints the first x rows from the top in the dataset
+
+# print(df1.tail(x)) Prints the last x rows from the bottom in the dataset
+
+# print(df1.shape) (7, 3)
