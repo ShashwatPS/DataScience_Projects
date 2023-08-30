1c5bb95ae511c3473171a04dce4fffd4a58e415a
Shashwat Singh <shashwat123student@gmail.com>
2023-08-31 01:41:09 +0530

    Fixed Questions

--- DataSets_Analysis/Adult Analysis.py
+++ DataSets_Analysis/Adult Analysis.py
@@ -2,30 +2,12 @@ import pandas as pd
 
 data = pd.read_csv('../DataSets/Adult.csv')
 
-# 1.  Display Top 10 Rows of The Dataset
-print(data.head(10))
-
+# 1.Display Top 10 Rows of The Dataset
 # 2. Check Last 10 Rows of The Dataset
-print(data.tail())
-
 # 3. Find Shape of Our Dataset (Number of Rows And Number of Columns)
-
-
-# 4.  Getting Information About Our Dataset Like Total Number Rows, Total Number of Columns, Datatypes of Each Column And Memory Requirement
-# 5. Check Null Values In The Dataset
-# 6. Drop ID, Notes, Agency, and Status Columns
-# 7. Get Overall Statistics About The Dataframe
-# 8. Find Occurrence of The Employee Names  (Top 5)
-# 9. Find The Number of Unique Job Titles
-# 10.Total Number of Job Titles Contain Captain
-# 11. Display All the Employee Names From Fire Department
-# 12. Find Minimum, Maximum, and Average BasePay
-# 13. Replace 'Not Provided' in EmployeeName' Column to NaN
-# 14. Drop The Rows Having 5 Missing Values
-# 15. Find Job Title of ALBERT PARDINI
-# 16. How Much ALBERT PARDINI Make (Include Benefits)?
-# 17.Display Name of The Person Having The Highest BasePay
-# 18.Find Average BasePay of All Employee Per Year
-# 19. Find Average BasePay of All Employee Per JobTitle
-# 20. Find Average BasePay of Employee Having Job Title ACCOUNTANT
-# 21. Find Top 5 Most Common Jobs
+# 4. Getting Information About Our Dataset Like Total Number Rows, Total Number of Columns, Datatypes of Each Column And Memory Requirement
+# 5. Fetch Random Sample From the Dataset (50%)
+# 6.Check Null Values In The Dataset
+# 7.Perform Data Cleaning [ Replace '?' with NaN ]
+# 8. Drop all The Missing Values
+# 9. Check For Duplicate Data and Drop Them
